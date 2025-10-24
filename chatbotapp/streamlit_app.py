7b1c83a735f7f942fa6b2f669b0544eac4889c4f
aryanjuyal <aryanjuyal70@gmail.com>
2025-10-24 18:21:47 +0530

    chat like features

--- chatbotapp/streamlit_app.py
+++ chatbotapp/streamlit_app.py
@@ -1,21 +1,16 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 from bot import gemini
-st.title("gemini chat bot")
 
+st.title("Gemini Chat Bot")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
-    
 
 
 for msg in st.session_state.chat_history:
-    if msg["role"] == "user":
-        st.write(f" **USer:** {msg['content']}")
-    else:
-        st.write(f" **Gemini:** {msg['content']}")
+    st.chat_message(msg["role"]).write(msg["content"])
 
 
 prompt = st.text_input("Ask Gemini something:")
@@ -27,13 +22,10 @@ if uploaded_image:
 if st.button("Ask Gemini"):
     if prompt or uploaded_image:
         with st.spinner("Thinking..."):
-      
             response = gemini(prompt if prompt else "Explain this image to me", uploaded_image)
 
-          
             st.session_state.chat_history.append({"role": "user", "content": prompt if prompt else 'Image'})
-            st.session_state.chat_history.append({"role": "gemini", "content": response})
-
+            st.session_state.chat_history.append({"role": "assistant", "content": response})
 
             st.success(response)
     else:
