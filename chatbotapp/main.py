11b9c3f681a7ad7ad1bb50c585de16a39b2d7bb5
aryanjuyal <aryanjuyal70@gmail.com>
2025-10-24 17:03:20 +0530

    Add base endpoint to bot API

--- chatbotapp/main.py
+++ chatbotapp/main.py
@@ -3,6 +3,11 @@ from .bot import gemini
 
 app = FastAPI()
 
+@app.get("/")
+def base():
+    return {"message": "This is my bot api"}
+
+
 @app.post("/chat")
 async def chat(prompt: str = Form(...), image: UploadFile = File(None)):
     if image:
